cda2c526c2f8ec130f5b43d052da10ded3e28126
Anna Volkova <annie_v@bk.ru>
2017-06-11 20:31:29 +0300

    script

---
+++ public/script.js
@@ -0,0 +1,83 @@
+document.addEventListener('DOMContentLoaded', VkInit);
+function VkInit(){
+  VK.init({
+    apiId: 6069953
+  }, false, false);
+
+  setTimeout(function() {
+    VK.Auth.getLoginStatus(VkLoginStatus, true)
+  }, 0)
+}
+
+
+const authBtn = document.querySelector('.js-auth');
+authBtn.addEventListener('click', function(event){
+  VK.Auth.login(VkLogin)
+})
+
+function VkLogin(data){
+  VkLoginStatus(data)
+}
+
+function VKUser(data) {
+  console.log(data);
+}
+
+function VkLoginStatus(data){
+  console.log('VkLoginStatus', data.session, data.status);
+  if (data.status === 'connected') {
+    const userName = document.querySelector('.js-user-name');
+
+    userName.innerText = data.session.user.first_name + ' ' + data.session.user.last_name
+
+    VK.api('friends.get',
+    {
+      user_id: data.session.user.id,
+      fields: 'first_name, last_name, user_id',
+    },
+    showFriends)
+    showLogoutButton();
+  } else {
+    cleanInfo();
+  }
+}
+
+function showFriends(data) {
+  const friends = data.response
+    .sort(compareRandom)
+    .slice(0, 5);
+  const friendsList = document.querySelector('.js-user-friends');
+  let HTML = '';
+  friends.forEach(function(friend){
+    HTML += `<li>
+      <a href="https://vk.com/id${friend.user_id}" target="_blank">
+        ${friend.first_name} ${friend.last_name}
+      </a>
+    </li>`
+  })
+  friendsList.innerHTML = HTML;
+}
+
+function compareRandom(a, b) {
+  return Math.random() - 0.5;
+}
+
+function showLogoutButton(){
+  const logoutBtn = document.querySelector('.js-logout');
+  logoutBtn.style.display='block';
+  logoutBtn.addEventListener('click', function(){
+    VK.Auth.logout();
+    setTimeout(function() {
+      VK.Auth.getLoginStatus(VkLoginStatus, true)
+    }, 1000)
+  })
+}
+
+function cleanInfo(){
+  const userName = document.querySelector('.js-user-name');
+  const friendsList = document.querySelector('.js-user-friends');
+  userName.innerText='';
+  friendsList.innerText='';
+  const logoutBtn = document.querySelector('.js-logout');
+  logoutBtn.style.display='none';
+}
